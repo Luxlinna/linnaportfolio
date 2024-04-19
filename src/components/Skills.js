090d78ef6723081a7bddc70dcf4c621e00a38d9a
Linna <lochlinna@gmail.com>
2024-04-19 10:18:33 +0200

    update icons on skill component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,18 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import icon1 from "../assets/icons/html.png";
+import icon2 from "../assets/icons/css.png";
+import icon3 from "../assets/icons/javascript.png";
+import icon4 from "../assets/icons/react.png";
+import icon5 from "../assets/icons/angular.png";
+import icon6 from "../assets/icons/node.png";
+import icon7 from "../assets/icons/mongodb.png";
+import icon8 from "../assets/icons/mysql.png";
+import icon9 from "../assets/icons/xd.png";
+import icon10 from "../assets/icons/git.png";
+import icon11 from "../assets/icons/github.png";
+import icon12 from "../assets/icons/figma.png";
+import icon13 from "../assets/icons/bootstrap.png";
+import icon14 from "../assets/icons/sass.png";
+import icon15 from "../assets/icons/agile.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
@@ -43,20 +55,64 @@ export const Skills = () => {
                         </p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Meter" />
-                                <h5>Frontend Developer</h5>
+                                <img src={icon1} alt="Icon" />
+                                <h5>HTML</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Meter" />
-                                <h5>UI/UX Design</h5>
+                                <img src={icon2} alt="Icon" />
+                                <h5>CSS</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Meter" />
-                                <h5>Logo Design</h5>
+                                <img src={icon3} alt="Icon" />
+                                <h5>JavaScript</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Meter" />
-                                <h5>Fullstack Developer</h5>
+                                <img src={icon4} alt="Icon" />
+                                <h5>React</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon5} alt="Icon" />
+                                <h5>Angular</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon6} alt="Icon" />
+                                <h5>Node.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon7} alt="Icon" />
+                                <h5>MongoDB</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon8} alt="Icon" />
+                                <h5>MySql</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon9} alt="Icon" />
+                                <h5>Adobe XD</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon10} alt="Icon" />
+                                <h5>Git</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon11} alt="Icon" />
+                                <h5>GitHub</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon12} alt="Icon" />
+                                <h5>Figma</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon13} alt="Icon" />
+                                <h5>Bootstrap</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon14} alt="Icon" />
+                                <h5>Sass</h5>
+                            </div>
+                            <div className="item">
+                                <img src={icon15} alt="Icon" />
+                                <h5>Agile</h5>
                             </div>
                         </Carousel>
                     </div>
