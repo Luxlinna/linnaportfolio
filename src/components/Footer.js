1252ea4509d21305cd15b53548eafdc86c1aa489
Luxlinna <luxlinna@gmail.com>
2024-10-06 23:42:21 +0700

    commit change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Navbar } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon2 from "../assets/img/nav-icon2.png";
+import navIcon3 from "../assets/img/nav-icon3.png";
 import ConnectMe from "./ConnectMe";
 
 export const Footer = () => {
@@ -19,8 +19,8 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/lux-linna-52203377/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://www.facebook.com/linna.loch.148" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/linnalux/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.figma.com/files/team/1294219165774754839/recents-and-sharing/recently-viewed?fuid=1294219163934603733" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.canva.com/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>&copy;Copyright 2024. All Rights Reserved</p>
           </Col>
